d4ebf23a2c2ec4f810a61565e1f8c8edc06af460
Joseph Gill <joseph@Josephs-MacBook-Pro.local>
2019-04-27 19:06:41 -0400

    update project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'titanwinds'`, () => {
+  it(`should have as title 'ui.titanwindsatl.com'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('titanwinds');
+    expect(app.title).toEqual('ui.titanwindsatl.com');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to titanwinds!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ui.titanwindsatl.com!');
   });
 });
