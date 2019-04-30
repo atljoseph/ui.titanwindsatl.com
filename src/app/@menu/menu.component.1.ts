364be368d3dc1256a0bf0fe89a1e94e493b35f58
Joseph Gill <joseph@Josephs-MacBook-Pro.local>
2019-04-30 00:59:25 -0400

    comment out extra module

--- src/app/@menu/menu.component.1.ts
+++ src/app/@menu/menu.component.1.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit, Inject, ViewEncapsulation, HostBinding } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-// import { MenuService } from './menu.service';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+// import { Component, OnInit, Inject, ViewEncapsulation, HostBinding } from '@angular/core';
+// import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+// // import { MenuService } from './menu.service';
+// import { trigger, state, style, transition, animate } from '@angular/animations';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
-  encapsulation: ViewEncapsulation.None, // for the "modal" action
-  animations: [
-    trigger('slideIn', [
-      state('void', style({ transform: 'translateY(-100%)' })),
-      state('enter', style({ transform: 'translateY(0)' })),
-      state('leave', style({ transform: 'translateY(-100%)' })),
-      transition('* => *', animate('1000ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
-    ])
-  ]
-})
-export class MenuComponent implements OnInit {
+// @Component({
+//   selector: 'app-menu',
+//   templateUrl: './menu.component.html',
+//   styleUrls: ['./menu.component.scss'],
+//   encapsulation: ViewEncapsulation.None, // for the "modal" action
+//   animations: [
+//     trigger('slideIn', [
+//       state('void', style({ transform: 'translateY(-100%)' })),
+//       state('enter', style({ transform: 'translateY(0)' })),
+//       state('leave', style({ transform: 'translateY(-100%)' })),
+//       transition('* => *', animate('1000ms cubic-bezier(0.25, 0.8, 0.25, 1)'))
+//     ])
+//   ]
+// })
+// export class MenuComponent implements OnInit {
 
-  // @HostBinding('@slideIn') slideDown = 'leave';
+//   // @HostBinding('@slideIn') slideDown = 'leave';
 
-  constructor(
-    // public menuService: MenuService,
-    private dialogRef: MatDialogRef<MenuComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+//   constructor(
+//     // public menuService: MenuService,
+//     private dialogRef: MatDialogRef<MenuComponent>,
+//     @Inject(MAT_DIALOG_DATA) public data: any
+//   ) { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-  closeMenu() {
-    this.dialogRef.close({ test: 'value' });
-  }
+//   closeMenu() {
+//     this.dialogRef.close({ test: 'value' });
+//   }
 
-}
+// }
--- src/app/@menu/menu.service.1.ts
+++ src/app/@menu/menu.service.1.ts
@@ -1,42 +1,42 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { MatDialog } from '@angular/material';
-import { MenuComponent } from './menu.component';
-import { IMenuModuleService } from './menu.types';
+// import { Injectable } from '@angular/core';
+// import { BehaviorSubject } from 'rxjs';
+// import { MatDialog } from '@angular/material';
+// import { MenuComponent } from './menu.component';
+// import { IMenuModuleService } from './menu.types';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class MenuService implements IMenuModuleService {
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class MenuService implements IMenuModuleService {
 
-  isMenuVisible: BehaviorSubject<boolean> = new BehaviorSubject(true);
+//   isMenuVisible: BehaviorSubject<boolean> = new BehaviorSubject(true);
 
-  constructor(
-    public dialogService: MatDialog
-  ) { 
-    this.isMenuVisible.subscribe(isVisible => {
-      console.log('isMenuVisible', isVisible);
-    });
-  }
+//   constructor(
+//     public dialogService: MatDialog
+//   ) { 
+//     this.isMenuVisible.subscribe(isVisible => {
+//       console.log('isMenuVisible', isVisible);
+//     });
+//   }
 
-  appOnInit() {
+//   appOnInit() {
     
-  }
+//   }
 
-  openMenu() {
-    this.isMenuVisible.next(true);
-    // open dialog
-    const dialogRef = this.dialogService.open(MenuComponent, {
-      panelClass: 'menu-overlay',
-      data: { test: 'data' },
-      hasBackdrop: false, // if true, shadow AND click off to dismiss
+//   openMenu() {
+//     this.isMenuVisible.next(true);
+//     // open dialog
+//     const dialogRef = this.dialogService.open(MenuComponent, {
+//       panelClass: 'menu-overlay',
+//       data: { test: 'data' },
+//       hasBackdrop: false, // if true, shadow AND click off to dismiss
       
-    });
-    // listen for the close event, and also result, if any
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('dialogRef.afterClosed', result);
-      this.isMenuVisible.next(false);
-    });
-  }
+//     });
+//     // listen for the close event, and also result, if any
+//     dialogRef.afterClosed().subscribe(result => {
+//       console.log('dialogRef.afterClosed', result);
+//       this.isMenuVisible.next(false);
+//     });
+//   }
   
-}
+// }
